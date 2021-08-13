ba2718ecb6b1907e7fe99c1b8bc87ac4993878d0
Zach Fey <zach.fey@parivedasolutions.com>
2021-08-13 09:09:37 -0400

    update accomodation and location

--- src/components/accommodations/accommodations.js
+++ src/components/accommodations/accommodations.js
@@ -1,27 +1,51 @@
 import React from 'react';
-import { Image, Container, Button, Card } from 'react-bootstrap';
-import pageImage from "../../images/engagement_photos/location1.jpg"
+import { Container, Button, Card, CardGroup } from 'react-bootstrap';
 require('./accommodations.scss')
 export default function Location() {
     return (
         <Container fluid className="accommodations-container">
             <div id="accommodations">
-            <Image src={pageImage} rounded fluid id="accommodations-image"/>
-                {/* <div className="overlay-top">
-                    <h1 className="text-center engaged">LOCATION</h1>
-                </div> */}
+                <h3>We have blocks of rooms with discounted rates at both of the following hotels. Shuttle pickup and dropoff will be at 
+                    the Moxy, but the two hotels are very close to each other.
+                </h3>
             </div>
-            <Card className="accommodations-card" style={{ width: '18rem' }}>
-                {/* <Card.Img variant="top" src="https://oakleafcottage.com/storage/app/media/assets/Oakleaf-Cottage-Chattanooga.png" style={{ 'background-color': '#393F2D', 'padding': '10px' }} /> */}
-                <Card.Body>
-                    <Card.Title>MOXY Chattanooga Downtown</Card.Title>
-                    <Card.Text>
-                        <p className="address">1220 King Street <br /> Chattanooga, Tennessee 37403</p>
-
-                    </Card.Text>
-                    <Button href="https://www.marriott.com/event-reservations/reservation-link.mi?id=1618333626360&key=GRP&app=resvlink" target="_blank">Reserve Room</Button>
-                </Card.Body>
-            </Card>
+            <CardGroup>
+                <Card className="accommodations-card" style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://cache.marriott.com/marriottassets/marriott/CHAOX/chaox-guestroom-5829-hor-feat.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1180px:*" />
+                    <Card.Body>
+                        <Card.Title>MOXY Chattanooga Downtown</Card.Title>
+                        <Card.Text>
+                            <p className="address">1220 King Street <br /> Chattanooga, Tennessee 37403</p>
+                            <ul>
+                                <li>Complementary beverage provided upon arrival.</li>
+                                <li>Valet or self parking is $15/night.</li>
+                                <li>Shuttle Pickup and Dropoff is here</li>
+                            </ul>
+                        </Card.Text>
+                        <Button href="https://www.marriott.com/event-reservations/reservation-link.mi?id=1618333626360&key=GRP&app=resvlink" target="_blank">Reserve Room</Button>
+                        <div className="map-box">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1633.3382217306846!2d-85.31019794175525!3d35.03982349508723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88605fe8761cce13%3A0xca45ed9b3732da0b!2sMoxy%20Chattanooga%20Downtown!5e0!3m2!1sen!2sus!4v1628812074255!5m2!1sen!2sus" title="moxy" width="400" height="300" style={{border:0}} allowfullscreen=""  loading="lazy"></iframe>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Card className="accommodations-card" style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="https://www.hilton.com/im/en/CHABSQQ/15040308/standard-king-room-01.jpg?impolicy=crop&cw=4500&ch=1889&gravity=NorthWest&xposition=0&yposition=555&rw=1920&rh=806"/>
+                    <Card.Body>
+                        <Card.Title>The Chattanoogan</Card.Title>
+                        <Card.Text>
+                            <p className="address">1201 Broad Street <br /> Chattanooga, Tennessee 37402</p>
+                            <ul>
+                                <li>Self parking $9/night or valet parking $18/night</li>
+                                <li>Shuttle pickup and dropoff will be at the Moxy which is about a 7 min walk (0.3 miles)</li>
+                            </ul>
+                        </Card.Text>
+                        <Button href="TODO: Update with link" target="_blank">Reserve Room</Button>
+                        <div className="map-box">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1633.3382217306846!2d-85.31019794175525!3d35.03982349508723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88605e7e93bd54df%3A0xcebd61bec2391923!2sThe%20Chattanoogan%20Hotel%2C%20Curio%20Collection%20by%20Hilton!5e0!3m2!1sen!2sus!4v1628811793489!5m2!1sen!2sus" title="chattanoogan" width="400" height="300" style={{border:0}} allowfullscreen="" loading="lazy"></iframe>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </CardGroup>
         </Container>
     )
 }
--- src/components/location/location.js
+++ src/components/location/location.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import { Image, Container, Button, Card } from 'react-bootstrap';
-import pageImage from "../../images/engagement_photos/location2.jpg"
+import pageImage from "../../images/oakleafCottageBanner.jpg"
 require('./location.scss')
 export default function Location() {
     return (
         <Container fluid className="location-container">
             <div id="location">
-            <Image src={pageImage} rounded fluid id="location-image"/>
+                <Image src={pageImage}></Image>
+            {/* <Image src={pageImage} rounded fluid id="location-image"/> */}
                 {/* <div className="overlay-top">
                     <h1 className="text-center engaged">LOCATION</h1>
                 </div> */}
+                {/* <h1>Location</h1> */}
             </div>
             <Card className="location-card" style={{ width: '18rem' }}>
                 {/* <Card.Img variant="top" src="https://oakleafcottage.com/storage/app/media/assets/Oakleaf-Cottage-Chattanooga.png" style={{ 'background-color': '#393F2D', 'padding': '10px' }} /> */}
                 <Card.Body>
-                    <Card.Title>Oakleaf Cottage</Card.Title>
+                    {/* <Card.Title></Card.Title> */}
                     <Card.Text>
-                        <h5>4:00pm</h5>
-                        <p className="address">6979 GA-136 <br /> Trenton, GA 30752</p>
+                        <p>The ceremony and reception will be held at</p>
+                        <h4>Oakleaf Cottage</h4>
+                        <h5>5:00pm</h5>
+                        <p className="address">6979 GA-136<br/>Trenton, GA 30752</p>
+                        <Button href="https://g.page/OakleafCottage?share" target="_blank">View on Map</Button>
+                        <p><i>Transportation to and from the venue from the Moxy Hotel will be provided.</i></p>
 
                     </Card.Text>
-                    <Button href="https://g.page/OakleafCottage?share" target="_blank">View on Map</Button>
                 </Card.Body>
             </Card>
         </Container>
