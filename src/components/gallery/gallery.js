e3b0944c5dd0969f3a03dad4ff13d615f6df5fe5
Zach Fey <zach.fey@parivedasolutions.com>
2021-08-25 09:32:03 -0400

    remove extra images, move gallery images to s3

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Container, Carousel } from 'react-bootstrap';
-import image1 from '../../images/gallery/EngagementPhotos-2.jpg';
-import image2 from '../../images/gallery/EngagementPhotos-24.jpg';
-import image3 from '../../images/gallery/EngagementPhotos-73.jpg';
-import image4 from '../../images/gallery/EngagementPhotos-120.jpg';
-import image5 from '../../images/gallery/EngagementPhotos-130.jpg';
-import image6 from '../../images/gallery/EngagementPhotos-141.jpg';
-import image7 from '../../images/gallery/EngagementPhotos-168.jpg';
-import image8 from '../../images/gallery/EngagementPhotos-173.jpg';
-import image9 from '../../images/gallery/EngagementPhotos-198.jpg';
+const image1 = 'https://engagement-photos-zach-and-katie.s3.amazonaws.com/EngagementPhotos-2.jpg';
+const image2 = 'https://engagement-photos-zach-and-katie.s3.amazonaws.com/EngagementPhotos-24.jpg';
+const image3 = 'https://engagement-photos-zach-and-katie.s3.amazonaws.com/EngagementPhotos-73.jpg';
+const image4 = 'https://engagement-photos-zach-and-katie.s3.amazonaws.com/EngagementPhotos-120.jpg';
+const image5 = 'https://engagement-photos-zach-and-katie.s3.amazonaws.com/EngagementPhotos-130.jpg';
+const image6 = 'https://engagement-photos-zach-and-katie.s3.amazonaws.com/EngagementPhotos-141.jpg';
+const image7 = 'https://engagement-photos-zach-and-katie.s3.amazonaws.com/EngagementPhotos-168.jpg';
+const image8 = 'https://engagement-photos-zach-and-katie.s3.amazonaws.com/EngagementPhotos-173.jpg';
+const image9 = 'https://engagement-photos-zach-and-katie.s3.amazonaws.com/EngagementPhotos-198.jpg';
 require('./gallery.scss');
 export default function Location() {
     const carouselImages = [image1, image2, image3, image4, image5, image6, image7, image8, image9];
