e11561fddfbf039620de20c2ff7cb32934132202
Zach Fey <zach.fey@parivedasolutions.com>
2021-08-24 22:08:18 -0400

    updated dress code

--- src/components/dressCode/dressCode.js
+++ src/components/dressCode/dressCode.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Image } from 'react-bootstrap';
 require('./dressCode.scss')
 
 export default function DressCode() {
@@ -8,8 +8,10 @@ export default function DressCode() {
             <h2 className="page-header">Dress Code</h2>
             <div className="text-container">
                 <p>This is an outdoor venue and there will be gravel - please keep this in mind when choosing your footwear!</p>
-                <p>The dress code is:</p>
+                <p>We want everyone to be comfortable and looking sharp, so the dress code is: </p>
                 <h5>Dressy Casual</h5>
+                <p>Here are a few photos for inspiration. Feel free to wear a suit if you'd like!</p>
+                <Image fluid src='https://media-api.xogrp.com/images/cd77b698-7ebd-474a-8fdd-766d252252a6~rs_2560.h'/>
             </div>
         </Container>
         
